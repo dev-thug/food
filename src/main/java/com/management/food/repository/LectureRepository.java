fa29650e850853fad1cf98a9e9e4d637cb82d687
hayagou <de0978@gmail.com>
2021-09-30 00:30:54 +0900

    add Repository

--- src/main/java/com/management/food/repository/LectureRepository.java
+++ src/main/java/com/management/food/repository/LectureRepository.java
@@ -1,10 +1,10 @@
 package com.management.food.repository;
 
-import com.management.food.entity.Food;
+import com.management.food.entity.Lecture;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FoodRepository extends JpaRepository<Food, Long> {
-    Page<Food> findByNameContaining(String name, Pageable pageable);
+public interface LectureRepository extends JpaRepository<Lecture, Long> {
+    Page<Lecture> findByNameContaining(String name, Pageable pageable);
 }
---
+++ src/main/java/com/management/food/repository/TeacherRepository.java
@@ -0,0 +1,7 @@
+package com.management.food.repository;
+
+import com.management.food.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
